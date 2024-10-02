36929d4c465fe9d7d941ff4b8ca927247856d2fb
jurchiks33 <jurisslisans@gmail.com>
2024-10-02 10:34:58 +0200

    data gathering and resampling

--- M2 Money Supply.py
+++ M2 Money Supply.py
@@ -13,3 +13,17 @@ def get_m2_data(start, end):
 def get_stock_data(ticker, start, end):
     stock_data = yf.download(ticker, start=start, end=end)
     return stock_data['Adj Close']
+
+# defining range. Change as needed
+start_date = '2000-01-01'
+end_date = '2024-09-09'
+
+# getting M2 money supply data
+m2 = get_m2_data(start_date, end_date)
+
+# getting S&P 500 stock market data
+sp500 = get_stock_data('^GSPC', start_date, end_date)
+
+# resampling M2 and S&P 500 data to a monthly frequency and we merge them.
+m2_monthly = m2.resample('M').last()
+sp500_monthly = sp500.resample('M').last()
