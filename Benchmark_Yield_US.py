81dcb987457ae3f76f0efd944d76ecddb9c38d89
jurchiks33 <jurisslisans@gmail.com>
2024-10-02 17:21:43 +0200

    data merging

--- Benchmark_Yield_US.py
+++ Benchmark_Yield_US.py
@@ -8,3 +8,13 @@ def get_yield_data(start, end):
     dgs2 = web.DataReader('DGS2', 'fred', start, end)
     return dgs10, dgs2
 
+# time range for data retrieval
+start_date = '2000-01-01'
+end_date = '2024-09-09'
+
+# getting yield data
+dgs10, dgs2 = get_yield_data(start_date, end_date)
+
+# data merging into a single data frame
+data = pd.merge(dgs10, dgs2, left_index=True, right_index=True, how='inner')
+data.columns = ['10_Year', '2_Year']
