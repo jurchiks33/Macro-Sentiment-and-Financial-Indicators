367e913fcc00a7a8fd2060ad0b5e20112afb8c69
jurchiks33 <jurisslisans@gmail.com>
2024-10-07 16:41:11 +0200

    scraping logic

--- ism_report_scraper.py
+++ ism_report_scraper.py
@@ -10,3 +10,16 @@ def convert_to_float(text):
     except ValueError:
         return None
 
+# function to scrape ISM, adjust MONTH as needed
+def scrape_ism_report():
+    url = "https://www.ismworld.org/supply-management-news-and-reports/reports/ism-report-on-business/pmi/september/"
+
+    # send response to get a page content
+    response = requests.get(url)
+
+    # check if request succeed
+    if response.status_code != 200:
+        print(f"failed to get ISM repor. Status Code: {response.status_code}")
+        return None
+    
+    
