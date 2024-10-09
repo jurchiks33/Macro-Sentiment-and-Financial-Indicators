5901f3c1a731cb17f11e81733be1ab594acca648
jurchiks33 <jurisslisans@gmail.com>
2024-10-09 17:27:48 +0200

    data extraction

--- ism_report_on_business_SERVICES.py
+++ ism_report_on_business_SERVICES.py
@@ -49,4 +49,18 @@ table = soup.find('table')
 # extracting rows from the table
 rows = table.find_all('tr')
 
+# process from second row to shift all data one row up
+for i in range(1, len(index_names) + 1):
+    try:
+        cols = rows[i].find_all('td')
 
+        # extracting September and August data
+        if len(cols) >= 2:
+                sep_value = convert_to_float(cols[0].get_text(strip=True))  # September data
+                aug_value = convert_to_float(cols[1].get_text(strip=True))  # August data
+        else:
+                sep_value = np.nan
+                aug_value = np.nan
+    except IndexError:
+            sep_value = np.nan
+            aug_value = np.nan
