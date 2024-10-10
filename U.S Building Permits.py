2b21757ba7ba06505b7c35d29d0ec2184979ed34
jurchiks33 <jurisslisans@gmail.com>
2024-10-10 15:38:29 +0200

    plotting permits

--- U.S Building Permits.py
+++ U.S Building Permits.py
@@ -1,6 +1,6 @@
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
-import pandas as plt
+import pandas as pd
 
 # function to get U.S. building permits from FRED
 def fetch_building_permits_data():
@@ -8,10 +8,29 @@ def fetch_building_permits_data():
 
     # defining start and end data for our dataset
     start_date = '1980-01-01'
-    end_date = pd.to_datetime('today').strftime('%Y-%m_d')
+    end_date = pd.to_datetime('today').strftime('%Y_%m_d')
 
     #getting data from FRED
     df = web.DataReader(series_id, 'fred', start_date, end_date)
 
-    
+    # renaming collumns
+    df.columns = ['Building Permits']
 
+    return df
+
+# function to plot U.S Building Permits data
+def plot_building_permits(df):
+    plt.figure(figsize=(10, 6))
+    plt.plot(df.index, df['Building Permits'], 
+             label='Building Permits', 
+             color='blue', 
+             linewidth=2)
+    
+    # plotting
+    plt.title('U.S. Building Permits', fontsize=14)
+    plt.xlabel('Date')
+    plt.ylabel('Number of Permits')
+    plt.grid(True)
+    plt.legend(loc='best')
+    plt.tight_layout()
+    plt.show()
