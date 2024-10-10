e356880363201228bcd123d84543b3aa6ecaa758
jurchiks33 <jurisslisans@gmail.com>
2024-10-10 16:30:45 +0200

    preparing main application

---
+++ main_application.py
@@ -0,0 +1,6 @@
+import tkinter as tk
+from tkinter import ttk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
+# importing individual modules to the main file
