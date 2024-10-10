0534f0bfad03daf9b87c94460ad8e496a623ffe3
jurchiks33 <jurisslisans@gmail.com>
2024-10-10 16:54:15 +0200

    display functions

--- main_application.py
+++ main_application.py
@@ -12,3 +12,18 @@ import us_building_permits
 import umsci_scrapper
 import vix_analysis
 
+# function to display M2 Money Supply vs Stock Market Analysis
+def display_m2_money_supply():
+    clear_canvas()
+    M2_Money_Supply.main()
+
+# function to display U.S. buildng permits
+def display_us_buildings_permits():
+    clear_canvas()
+    us_building_permits.main()
+
+# function to display ISM report on business Services
+def display_ism_services_report():
+    clear_canvas()
+    ism_report_on_business_SERVICES.main()
+
