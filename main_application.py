eb9a87a6fcea0f7d15b33a347e12cff8e3b554e1
jurchiks33 <jurisslisans@gmail.com>
2024-10-10 16:48:21 +0200

    importing files needed

--- main_application.py
+++ main_application.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 # importing individual modules to the main file
+import Benchmark_Yield_US
+import ism_report_on_business_SERVICES
+import ism_report_scraper
+import M2_Money_Supply
+import us_building_permits
+import umsci_scrapper
+import vix_analysis
+
