86d4881274dd754f07081768975e016fe51efe1b
jurchiks33 <jurisslisans@gmail.com>
2024-10-12 18:37:23 +0200

    function for building permits

--- sp500_building_permits_ma.py
+++ sp500_building_permits_ma.py
@@ -7,3 +7,9 @@ import matplotlib.pyplot as plt
 def get_sp500_data(start, end):
     sp500_data = yf.download('^GSPC', start=start, end=end)
     return sp500_data['Adj Close']
+
+# Function to get U.S. Building Permits data (from FRED)
+def get_building_permits_data(start, end):
+    permits_data = web.DataReader('PERMIT', 'fred', start, end)
+    return permits_data
+
